38b0764ec00994927f0ce3f1953c1d2563a698b2
norellia <alexkiki3@me.com>
2024-08-30 19:43:50 +0100

    Clean and comment the cleaning.R script

--- cleaning.R
+++ cleaning.R
@@ -1,53 +1,47 @@
-setwd("D:/Documents/RSMAS/Bayesian/Project")
+##### Clean and Examine White Perch Otolith Data #####
 
-wtpa <- read.csv("all_bc.csv")
-
+# Load packages
 library(tidyverse)
 library(FSA)
 
-test <- pivot_longer(wtpa, c(ONE.L, TWO.L, THREE.L, FOUR.L, FIVE.L, SIX.L, SEVEN.L, EIGHT.L), names_to = "otolith", values_drop_na = TRUE)
-test1 <- test
-test1$otolith <-as.factor(test1$otolith)
-test1$otolith <- factor(test1$otolith, levels = c("ONE.L", "TWO.L", "THREE.L", "FOUR.L", "FIVE.L", "SIX.L", "SEVEN.L", "EIGHT.L"))
-test1$otolith <- as.numeric(test1$otolith)
+# Read in the white perch age data (wtpa)
+wtpa <- read.csv("all_bc.csv")
 
-test1$otolith
+# Reformat the back-calculated length at ages
+bc_wtpa <- pivot_longer(wtpa, c(ONE.L, TWO.L, THREE.L, FOUR.L, FIVE.L, SIX.L, SEVEN.L, EIGHT.L), names_to = "otolith", values_drop_na = TRUE)
+bc_wtpa$otolith <-as.factor(bc_wtpa$otolith)
+bc_wtpa$otolith <- factor(bc_wtpa$otolith, levels = c("ONE.L", "TWO.L", "THREE.L", "FOUR.L", "FIVE.L", "SIX.L", "SEVEN.L", "EIGHT.L"))
+bc_wtpa$otolith <- as.numeric(bc_wtpa$otolith)
 
-plot(wtpa$Age,wtpa$Length, xlab = "Age", ylab = "Total Length (cm)" )
-points(test1$otolith, test1$value)
+# Check that there are 8 factors
+unique(bc_wtpa$otolith)
 
-wtpa$test <- rep("Measured")
-test1$test <- rep("Back-Calc")
+# Add a type of measurement variable
+wtpa$type <- rep("Measured")
+bc_wtpa$type <- rep("Back-Calc")
 
-df1 <- data.frame("Length" = c(test1$value, wtpa$Length), "Age" = c(test1$otolith, wtpa$Age), "Site" = c(test1$Site, wtpa$Site), "BC" = c(test1$test, wtpa$test))
-plot(df1$Age, df1$Length)
-df1$BC <- as.factor(df1$BC)
-#histograms
-ggplot(df1, aes(Length, fill = BC)) +
+# Merge the two types of datasets with cleaned variables
+cleaned_wtpa <- data.frame("Length" = c(bc_wtpa$value, wtpa$Length), "Age" = c(bc_wtpa$otolith, wtpa$Age), "Site" = c(bc_wtpa$Site, wtpa$Site), "Type" = c(bc_wtpa$type, wtpa$type))
+cleaned_wtpa$Type <- as.factor(cleaned_wtpa$Type)
+
+# Histogram of Fish Length
+ggplot(cleaned_wtpa, aes(Length, fill = Type)) +
   geom_histogram(binwidth = 10, color = "black") + scale_fill_manual(values=c("white", "black")) +ggtitle("Histogram of Fish Length (cm)") + ylab("Count")+ theme_classic()
 
+# Histogram of Fish Ages
+ggplot(cleaned_wtpa, aes(Age, fill = Type)) +
+  geom_histogram(binwidth = 1, color = "black") + scale_fill_manual(values=c("white", "black"))+ggtitle("Histogram of Fish Age") + ylab("Count") + theme_classic()
 
-#hist(wtpa$Length, xlab = "Total Length (cm)", main = "Histogram of All Fish")
-#hist(df1$Length, xlab = "Total Length (cm)", main = "Histogram of All Fish and Back-Calculated Lengths")
-
-#raw data plotted
-ggplot(df1, aes(x = Age, y = Length, color = BC)) +
+# Scatterplot of raw length at age by type
+ggplot(cleaned_wtpa, aes(x = Age, y = Length, color = Type)) +
   geom_point() + scale_color_manual(values=c("#999999", "black"))
 
-#plot(wtpa$Age, wtpa$Length, xlab = "Age", ylab = "Total Length (cm)", main = "All Fish")
-#plot(df1$Age, df1$Length, xlab = "Age", ylab = "Total Length (cm)", main = "All Fish and Back-Calculations")
-
-#age histograms
-ggplot(df1, aes(Age, fill = BC)) +
-  geom_histogram(binwidth = 1, color = "black") + scale_fill_manual(values=c("white", "black"))+ggtitle("Histogram of Fish Age") + ylab("Count") + theme_classic()
-df1$round <- ceiling(df1$Age)
-ggplot(df1, aes(round, fill = BC)) +
-  geom_histogram(bins = 9, color = "black") + scale_fill_manual(values=c("white", "black"))+ggtitle("Histogram of Fish Age") + ylab("Count") + theme_classic()
-
-mes <- filter(df1, BC == "Measured")
+# Measured-type length at age by location
+mes <- filter(cleaned_wtpa, Type == "Measured")
 ggplot(mes, aes(x = Age, y = Length)) +
   geom_point(aes(shape = Site, color = Site))
 
-ggplot(df1, aes(x = Age, y = Length)) +
+# All length at ages by location
+ggplot(cleaned_wtpa, aes(x = Age, y = Length)) +
   geom_point(aes(shape = Site))
 
